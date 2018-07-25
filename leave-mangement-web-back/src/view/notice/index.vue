1a3358797703473567199486a5dcb6160437edff
1Joy <1298520866@qq.com>
2018-07-25 21:43:33 +0800

    具体实现

--- leave-mangement-web-back/src/view/notice/index.vue
+++ leave-mangement-web-back/src/view/notice/index.vue
@@ -17,25 +17,22 @@
             <el-table :data="tableData" :header-cell-style="headerCellStyle" border style="width: 100%">
                 <el-table-column align="center" prop="id" label="序号" width="80">
                 </el-table-column>
-                <el-table-column align="center" prop="workerName" label="公告名称" width="100">
+                <el-table-column align="center" prop="title" label="公告名称" >
                 </el-table-column>
-                <el-table-column align="center" prop="type" label="公告类型" width="120">
+                <el-table-column align="center" prop="type" label="公告类型" width="110">
                 </el-table-column>
-                <el-table-column align="center" prop="startTime" label="公告内容">
+                <el-table-column align="center" prop="content" label="公告内容">
                 </el-table-column>
-                <el-table-column align="center" prop="createTime" label="创建时间">
+                <el-table-column align="center" prop="createTime" width="110" label="创建时间">
                 </el-table-column>
-                <el-table-column align="center" prop="createHuman" label="创建人"></el-table-column>
+                <el-table-column align="center" prop="createHuman" width="100" label="创建人"></el-table-column>
                 <el-table-column align="center" label="操作" width="80">
                     <template slot-scope="scope">
                         <el-dropdown>
                             <i class="el-icon-menu"></i>
                             <el-dropdown-menu slot="dropdown">
                                 <el-dropdown-item>
-                                 <el-button type="text" size="small" @click="handleLook(scope.row.id)" icon="el-icon-view">查看</el-button>
-                                </el-dropdown-item>
-                                <el-dropdown-item>
-                                 <el-button @click="handleEdit(scope.row.id)" type="text" size="small" icon="el-icon-edit">编辑</el-button>
+                                 <el-button type="text" size="small" @click="handleLook(scope.row)" icon="el-icon-view">查看</el-button>
                                 </el-dropdown-item>
                                 <el-dropdown-item>
                                  <el-button @click="handleDelete(scope.row.id)" style="color:red;" type="text" size="small" icon="el-icon-delete">删除</el-button>
@@ -56,20 +53,42 @@
             :total="totalCount">
             </el-pagination>
         </div>
+        <el-dialog width="408px" :title="noticeData.title" :visible.sync="dialogVisible" :before-close="handleClose">
+         <comp-view @closeForm='handleClose' @close='closeForm' :notice="noticeData" ref="compView"></comp-view>
+        </el-dialog>
     </div>
 </template>
 <script>
 import '../index.scss'
+import CompView from './noticeview'
 export default {
+    components:{
+        CompView
+    },
     data(){
         return{
-            tableData: [],
+            tableData: [{
+                id:1,
+                title:'3333333333333',
+                type:'部门公告',
+                content:'wertyusdfghjkdfghjkfgh',
+                createTime:'2018-02-15',
+                createHuman:'姜鑫'
+            },{
+                id:2,
+                title:'3333333333333',
+                type:'部门公告',
+                content:'wertyusdfghjkdfghjkfgh',
+                createTime:'2018-02-15',
+                createHuman:'joy'
+            }],
             headerCellStyle: {
                 backgroundColor: '#f2f2f2',
                 fontSize: '14px',
                 color: '#434343',
                 fontWeight: 400
             },
+            noticeData:{},
             currentPage: 1,
             pageSize: 20,
             query: '',
@@ -88,18 +107,12 @@ export default {
             this.loadData();
         },
         //点击查看
-        handleLook(id){
-            this.applicationId = id
+        handleLook(data){
+            this.noticeData = data
             this.dialogVisible = true
         },
-        //点击编辑
-        handleEdit(id) {
-            this.$router.push({
-                path: "/addApplication/" + id
-            });
-        },
         handleDelete(id) {
-            this.$confirm('此操作将永久删除该申请, 是否继续?', '提示', {
+            this.$confirm('此操作将删除该公告, 是否继续?', '提示', {
                     confirmButtonText: '确定',
                     cancelButtonText: '取消',
                     type: 'warning'
@@ -115,6 +128,14 @@ export default {
         handleChangeQuery() {
             this.loadData()
         },
+        //关闭弹框
+        handleClose(){
+            this.dialogVisible = false
+        },
+        closeForm(val){
+            this.loadData()
+            this.dialogVisible = val
+        }, 
     }
 }
 </script>
---
+++ leave-mangement-web-back/src/view/notice/noticeview.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>
+            {{this.notice.content}}
+        </div>
+        <div style="text-align: right;">
+            <p>{{this.notice.createTime}}</p>
+            <p>{{this.notice.createHuman}}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:['notice']
+}
+</script>
